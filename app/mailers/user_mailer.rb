6f66aead2e7ca5f778397b092be80db76eebf373
DevYves <yfreypons@gmail.com>
2017-12-08 03:01:23 +0000

    added order status updates and mailer to send emails to user upon purchase

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,12 @@
+class UserMailer < ApplicationMailer
+  default from: "no-reply@jungle.com"
+
+  def order_receipt_email(order)
+    @order = order
+     mail(to: @order.email, subject: "Jungle Order ID: #{@order.id}", template_path: 'user_mailer', template_name: 'receipt')
+
+  end
+
+
+end
+
