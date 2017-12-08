269763a1dccccef8ef5b5e41420f40e4798a0c41
DevYves <yfreypons@gmail.com>
2017-12-08 04:19:22 +0000

    added basic authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,34 +1,34 @@
 Rails.application.routes.draw do
 
-
-
   root to: 'products#index'
 
-  resources :products, only: [:index, :show]
+  resources :products, only: [:index, :show] do
+    resources :reviews, only: [:create]
+  end
+
   resources :categories, only: [:show]
-
-  get '/login' => 'sessions#new'
-  post '/login' => 'sessions#create'
-  get '/logout' => 'sessions#destroy'
-
-  get '/signup' => 'users#new'
-  post '/users' => 'users#create'
 
   resource :cart, only: [:show] do
     put    :add_item
     delete :remove_item
   end
 
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+
+  get '/users', to: 'users#index'
+  get '/register', to: 'users#new'
+  post '/users', to: 'users#create'
+
+
   resources :orders, only: [:create, :show]
 
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
-    resources :categories, only: [:index, :new, :create]
-
+    resources :categories, except: [:edit, :update, :show]
   end
-
-
 
 
 
