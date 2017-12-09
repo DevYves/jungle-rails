60f5fee114df930f3da201a7e94c89c2fc2b5fc2
DevYves <yfreypons@gmail.com>
2017-12-09 02:03:25 +0000

    rebuilt controllers to fix user/review bug and added review controller

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,20 @@
+class ReviewsController < ApplicationController
+  def create
+    @review = Review.new review_params
+    @review.user_id = @user || nil
+    @review.product_id = params[:product_id]
+    @review.save
+
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  private
+    def review_params
+      params.require(:review).permit(:rating, :description)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,33 +2,36 @@ Rails.application.routes.draw do
 
   root to: 'products#index'
 
-  resources :products, only: [:index, :show] do
+   resources :products, only: [:index, :show] do
     resources :reviews, only: [:create]
   end
+  resources :reviews, only: [:edit, :destroy]
 
   resources :categories, only: [:show]
+  resources :orders, only: [:create, :show]
 
   resource :cart, only: [:show] do
     put    :add_item
     delete :remove_item
   end
 
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
-  get '/logout', to: 'sessions#destroy'
-
-  get '/users', to: 'users#index'
-  get '/signup', to: 'users#new'
-  post '/users', to: 'users#create'
 
 
-  resources :orders, only: [:create, :show]
+  resources :users, only: [:create, :new]
+  get '/signup' => 'users#new'
+
+  resources :sessions, only: [:new, :create, :destroy]
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
 
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
-    resources :categories, except: [:edit, :update, :show]
+    resources :categories, only: [:index, :create, :new]
   end
+
+
 
 
 
