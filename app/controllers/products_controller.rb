9ebe28ec4a67f635c796a1e0550bd35053010d64
DevYves <yfreypons@gmail.com>
2017-12-10 19:18:41 +0000

    added average rating to product page and additional styling

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,6 +2,7 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.all.order(created_at: :desc)
+
   end
 
   def show
@@ -9,5 +10,6 @@ class ProductsController < ApplicationController
     @reviews = @product.reviews
     @review = Review.new
   end
+
 
 end
